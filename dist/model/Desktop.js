6c2882c3f2282d530b25019b8a930ce5e36263e0
Rian Custódio <88561288+Rian1432@users.noreply.github.com>
2024-07-31 14:14:55 -0300

    Delete dist directory

--- dist/model/Desktop.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Computer_1 = require("./Computer");
-class Desktop extends Computer_1.default {
-}
-exports.default = Desktop;
