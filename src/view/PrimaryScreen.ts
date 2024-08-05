cf85f5305aac791ae2c362e0d289fa7e998fa621
Rian Custódio <88561288+Rian1432@users.noreply.github.com>
2024-08-05 19:42:11 -0300

    feat: Cria computador desktop

--- src/view/PrimaryScreen.ts
+++ src/view/PrimaryScreen.ts
@@ -1,46 +1,50 @@
 import promptSync from 'prompt-sync';
 import DesktopController from '../control/DesktopController';
 import Desktop from '../model/Desktop';
+import { useTypesEnum } from '../enums/UseTypes';
 
 export default class PrimaryScreen{
 
 	private DesktopController: DesktopController;
+	private prompt = promptSync({sigint: true});
 
 	constructor(DesktopController: DesktopController){
 		this.DesktopController = DesktopController;
 	}
-	private prompt = promptSync();
 
 	public getFirstScreen():void {
-        
-       
-		let showScreen: boolean = false;
-		while (!showScreen) {
-			const choice = this.prompt('Escolha:\n1 - Computadores\n2 - Vendedores\n3 - Sair');
-              
+
+		let showScreen: boolean = true;
+		while (showScreen) {
+			const choice = this.prompt('Escolha:\n1 - Computadores\n2 - Vendedores\n3 - Sair\n Opção escolhida: ');
+
 			switch (choice) {
 			case '1':
 				this.registerDesktop(this.DesktopController.getNewDesktop());
 				break;
-        
+
 			case '2':
 				console.log('digitou 2');
-                
 				break;
+
 			case '3':
-				showScreen = true;
+				showScreen = false;
 				break;
+
 			default:
-				console.log('Invalid answer!');
+				console.log('Resposta inválida!');
 			}
 		}
-      
-
 	}
 
 	public registerDesktop(Desktop: Desktop): void{
-		const name = this.prompt('Digite o nome do modelo');
+		const name = this.prompt('Digite o nome do modelo: ');
+		const price = parseInt(this.prompt('Digite o preço: '));
+
 		Desktop.setModelName(name);
+		Desktop.setPrice(price);
+		Desktop.setUseType(useTypesEnum.Basic);
+
 		this.DesktopController.registerDesktop(Desktop);
 		console.log(this.DesktopController.getDesktops());
 	}
